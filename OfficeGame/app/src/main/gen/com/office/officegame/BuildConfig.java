413377efb29851e9aedc50c025e97748fbf7b6f7
Andrii <ayakubenko92@gmail.com>
2014-07-02 23:14:52 +0300

    test

---
+++ OfficeGame/app/src/main/gen/com/office/officegame/BuildConfig.java
@@ -0,0 +1,10 @@
+/*___Generated_by_IDEA___*/
+
+package com.office.officegame;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+
+    //dsdsdf
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
