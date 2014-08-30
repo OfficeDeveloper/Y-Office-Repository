781c4a843653981834b3b918254123f92c8a1933
Andrii <ayakubenko92@gmail.com>
2014-08-30 13:49:20 +0300

    splashscreen upd.

--- OfficeGame/app/src/main/java/com/office/officegame/SplashScreen.java
+++ OfficeGame/app/src/main/java/com/office/officegame/SplashScreen.java
@@ -2,8 +2,10 @@ package com.office.officegame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.ImageView;
 
 /**
  * Created by Andrii on 8/26/14.
@@ -21,6 +23,17 @@ public class SplashScreen extends Activity {
 
         // Splash screen view
         setContentView(R.layout.splash);
+
+
+        final ImageView splashImageView = (ImageView) findViewById(R.id.SplashImageView);
+        splashImageView.setBackgroundResource(R.drawable.flag);
+        final AnimationDrawable frameAnimation = (AnimationDrawable)splashImageView.getBackground();
+        splashImageView.post(new Runnable(){
+            @Override
+            public void run() {
+                frameAnimation.start();
+            }
+        });
 
         final SplashScreen sPlashScreen = this;
 
