8ae2b70db671dc62775d84302fd4a008c9af5125
Gavlovich Maksim <reverff@gmail.com>
2014-09-30 01:34:26 +0300

    new design

--- OfficeGame/app/src/main/java/com/office/officegame/Firstgame.java
+++ OfficeGame/app/src/main/java/com/office/officegame/Firstgame.java
@@ -1,7 +1,6 @@
 package com.office.officegame;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
