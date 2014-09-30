8be33a7cfc6f79087bd17025b35b2462e3657e3d
Gavlovich Maksim <reverff@gmail.com>
2014-09-30 20:51:25 +0300

    fixed buttons

--- OfficeGame/app/src/main/java/com/office/officegame/Firstgame.java
+++ OfficeGame/app/src/main/java/com/office/officegame/Firstgame.java
@@ -99,14 +99,6 @@ public class Firstgame extends Activity implements View.OnClickListener, OnTouch
         b.setText("START");
     }
 
-    public void onResume() {
-        super.onResume();
-    }
-
-    public void onStop() {
-        super.onStop();
-    }
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firstgame);
