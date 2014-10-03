472a4a00557d01052c8bed50ae3ebe8131f2544a
Gavlovich Maksim <reverff@gmail.com>
2014-10-04 02:53:52 +0300

    info activity

--- OfficeGame/app/src/main/java/com/office/officegame/Info.java
+++ OfficeGame/app/src/main/java/com/office/officegame/Info.java
@@ -5,20 +5,24 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ViewAnimator;
 
 public class Info extends Activity implements View.OnClickListener, View.OnTouchListener {
     ViewAnimator views;
+    Game info;
     TextView arcadeGames, arcadeSh, arcadeHs;
+    TextView tsGames, tsSh, tsHs;
+    TextView taGames, taSh, taHs;
+    TextView sumGames, sumSh, sumHs;
+    TextView arcadeNext, arcadePrev, tsNext, tsPrev, taNext, taPrev, sumNext, sumPrev;
 
-    void swipeLeft(){
+    void swipeNext(){
         views.setInAnimation(this, R.anim.in_right);
         views.setOutAnimation(this, R.anim.out_left);
         views.showNext();
     }
 
-    void swipeRight(){
+    void swipePrev(){
         views.setInAnimation(this, R.anim.in_left);
         views.setOutAnimation(this, R.anim.out_right);
         views.showPrevious();
@@ -36,11 +40,77 @@ public class Info extends Activity implements View.OnClickListener, View.OnTouch
         arcadeSh = (TextView) findViewById(R.id.arcadeSh);
         arcadeGames = (TextView) findViewById(R.id.arcadeGames);
 
-        Game info = new Game(this);
+        tsHs = (TextView) findViewById(R.id.tsHs);
+        tsSh = (TextView) findViewById(R.id.tsSh);
+        tsGames = (TextView) findViewById(R.id.tsGames);
+
+        taHs = (TextView) findViewById(R.id.taHs);
+        taSh = (TextView) findViewById(R.id.taSh);
+        taGames = (TextView) findViewById(R.id.taGames);
+
+        sumHs = (TextView) findViewById(R.id.sumHs);
+        sumSh = (TextView) findViewById(R.id.sumSh);
+        sumGames = (TextView) findViewById(R.id.sumGames);
+
+        arcadeNext = (TextView) findViewById(R.id.arcadeNext);
+        arcadeNext.setOnClickListener(this);
+
+        arcadePrev = (TextView) findViewById(R.id.arcadePrev);
+        arcadePrev.setOnClickListener(this);
+
+        tsNext = (TextView) findViewById(R.id.tsNext);
+        tsNext.setOnClickListener(this);
+
+        tsPrev = (TextView) findViewById(R.id.tsPrev);
+        tsPrev.setOnClickListener(this);
+
+        taNext = (TextView) findViewById(R.id.taNext);
+        taNext.setOnClickListener(this);
+
+        taPrev = (TextView) findViewById(R.id.taPrev);
+        taPrev.setOnClickListener(this);
+
+        sumNext = (TextView) findViewById(R.id.sumNext);
+        sumNext.setOnClickListener(this);
+
+        sumPrev = (TextView) findViewById(R.id.sumPrev);
+        sumPrev.setOnClickListener(this);
+
+
+        info = new Game(this);
         info.connectDb();
+        info.createPlayer();
+
+        int hs1 = info.getHighScore(1);
+        int hs2 = info.getHighScore(2);
+        int hs3 = info.getHighScore(3);
+
         arcadeGames.setText(String.valueOf(info.getGamesCount(1)));
-        arcadeHs.setText(String.valueOf(info.getHighScore(1)));
+        arcadeHs.setText(String.valueOf(hs1));
         arcadeSh.setText(String.valueOf(info.getSumScore(1)));
+
+        tsGames.setText(String.valueOf(info.getGamesCount(2)));
+        tsHs.setText(String.valueOf(hs2));
+        tsSh.setText(String.valueOf(info.getSumScore(2)));
+
+        taGames.setText(String.valueOf(info.getGamesCount(3)));
+        taHs.setText(String.valueOf(hs3));
+        taSh.setText(String.valueOf(info.getSumScore(3)));
+
+        String sg = String.valueOf(info.getGamesCount(1)+info.getGamesCount(2)+info.getGamesCount(3));
+        sumGames.setText(sg);
+
+        int shs=0;
+        if (hs1>=hs2 && hs1>=hs3) shs = hs1;
+        if (hs2>=hs1 && hs2>=hs3) shs = hs2;
+        if (hs3>=hs1 && hs3>=hs2) shs = hs3;
+
+        sumHs.setText(String.valueOf(shs));
+
+        String ssh = String.valueOf(info.getSumScore(1)+info.getSumScore(2)+info.getSumScore(3));
+        sumSh.setText(ssh);
+
+        //sumGames
 
         views.setOnTouchListener(this);
 
@@ -48,8 +118,32 @@ public class Info extends Activity implements View.OnClickListener, View.OnTouch
 
     @Override
     public void onClick(View v) {
-
-        Toast.makeText(getBaseContext(), "CLICK",Toast.LENGTH_SHORT).show();
+        switch (v.getId()) {
+            case R.id.arcadeNext:
+                swipeNext();
+                break;
+            case R.id.arcadePrev:
+                swipePrev();
+                break;
+            case R.id.tsNext:
+                swipeNext();
+                break;
+            case R.id.tsPrev:
+                swipePrev();
+                break;
+            case R.id.taNext:
+                swipeNext();
+                break;
+            case R.id.taPrev:
+                swipePrev();
+                break;
+            case R.id.sumNext:
+                swipeNext();
+                break;
+            case R.id.sumPrev:
+                swipePrev();
+                break;
+        }
     }
     float startX;
     @Override
@@ -60,11 +154,29 @@ public class Info extends Activity implements View.OnClickListener, View.OnTouch
                 break;
             case MotionEvent.ACTION_UP:
                 float endX = e.getX();
-                if(startX > endX) {swipeLeft();}
-                if(startX < endX) {swipeRight();}
+                if(startX > endX) {
+                    swipeNext();}
+                if(startX < endX) {
+                    swipePrev();}
                 return false;
             default: break;
         }
         return true;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        info.pauseMusic();
+    }
+
+    public void onResume() {
+        super.onResume();
+        info.startMusic();
+    }
+
+    public void onStop() {
+        super.onStop();
+        info.stopMusic();
     }
 }
