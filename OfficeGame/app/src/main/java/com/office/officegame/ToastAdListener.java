183979f822877b5c301977416fb0fb9af942762e
Andrii <ayakubenko92@gmail.com>
2014-10-03 19:26:50 +0300

    mini upd. voice in game volume >> 50%

--- OfficeGame/app/src/main/java/com/office/officegame/ToastAdListener.java
+++ OfficeGame/app/src/main/java/com/office/officegame/ToastAdListener.java
@@ -2,7 +2,6 @@ package com.office.officegame;
 
 import android.content.Context;
 import android.widget.Toast;
-
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 
